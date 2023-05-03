f42e9bf26b9683151fc927f15d2a9c26f25a0cd2
Evgeniy-Shtykhlin-EPAM <Evgeniy_Shtykhlin@epam.com>
2023-05-03 14:10:25 +0600

    Add project files.

---
+++ Task1/Product.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task1
+{
+    public class Product
+    {
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var item = obj as Product;
+
+            if (item == null)
+            {
+                return false;
+            }
+
+            if (Name.Equals(item.Name) && Price.Equals(item.Price))
+            {
+                return true;
+            }
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name, Price);
+        }
+    }
+}
